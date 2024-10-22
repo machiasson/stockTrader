650523c8315495eac000cbcc4143590715aa3961
Christian Ziobro <ziobro44@gmail.com>
2024-10-22 14:48:00 -0700

    simple gui displays dropdown ticker symbol

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import yfinance as yf
 import json
 from datetime import datetime
-
+import tkinter as tk
+from tkinter import ttk
 
 # Define the stock symbols and date range
 stock_symbols = ['FNGU', 'FNGD']
@@ -14,6 +15,25 @@ def fetch_historical_data(symbol, start, end):
     stock_data = yf.download(symbol, start=start, end=end)
     return stock_data
 
+class Window:
+
+    def __init__(self):
+        # Initialize tkinter
+        self.root = tk.Tk()
+        self.root.geometry("500x500")
+
+        # Selection box for symbols
+        self.selectionBox = ttk.Combobox(state="readOnly", values=stock_symbols)
+        self.selectionBox.place(x=10, y=50)
+
+        #Display start and end dates
+        self.start_label = tk.Label(self.root, text="Start Date: " + start_date)
+        self.end_label = tk.Label(self.root, text="End date: " + end_date)
+        self.start_label.place(x=10,y=10)
+        self.end_label.place(x=10, y=30)
+
+        # Loop for tkinter
+        self.root.mainloop()
 
 # Fetch data for both symbols and store in a dictionary
 data = {}
@@ -32,3 +52,4 @@ for symbol in stock_symbols:
 
 
     print(f"Data for {symbol} saved to {output_file}")
+    Window()
